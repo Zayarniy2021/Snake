cbdfb8459b9dcc94c481a4cdd75e603ce8956bc6
Иванов Иван <zaazaa@yandex.ru>
2021-12-10 15:25:31 +0300

    Готовые передвижения

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -12,22 +12,55 @@ int headY;
 int[,] GameField;
 int w = 80, h = 40;
 
+//Заставка
+void SplashScreen()
+{
+    //for (int i = 0;i<=w;i++)
+    //{
+    //    Console.SetCursorPosition(i, 0);
+    //    Console.Write('#');
+    //    Console.SetCursorPosition(i, h+1);
+    //    Console.Write('#');
+    //}
+    //for (int i = 0; i <= h; i++)
+    //{
+    //    Console.SetCursorPosition(0, i);
+    //    Console.Write('#');
+    //    Console.SetCursorPosition(w+1, i);
+    //    Console.Write('#');
+    //}
+
+    for (int i = 0; i <= w; i++)
+    {
+        GameField[i, 0] = 10000;
+        GameField[i, h] = 10000;
+    }
+    for (int i = 0; i < h; i++)
+    {
+        GameField[0, i] = 10000;
+        GameField[w, i] = 10000;
+    }
+
+
+
+}
+
 void Init()
 {
     Console.CursorVisible = false;
-    Console.SetWindowSize(w+2, h+2);
-    Console.SetBufferSize(w+2, h+2);
+    Console.SetWindowSize(w+1, h+2);
+    Console.SetBufferSize(w+1, h+2);
     vx = 0;
     vy = 1;
     headX = 20;
     headY = 10;
-    GameField = new int[82, 42];
+    GameField = new int[w+1, h+1];
     GameField[headX, headY] = 1;
     //GameField[headX, headY - 1] = 2;
     //GameField[headX, headY - 2] = 3;
     //GameField[headX, headY - 3] = 4;
     Random random = new Random();
-    for(int i=0;i<10;i++)
+    for(int i=0;i<10000;i++)
         GameField[random.Next(80), random.Next(40)] =-1 ;
     PrintGameField();
     timer.Interval = 500;
@@ -58,7 +91,7 @@ void Update()
 bool Collision()
 {
     if (GameField[headX, headY] > 0) quit = true;
-    if (headX < 1 || headX > w || headY < 1 || headY > h) quit = true;
+    if (headX < 1 || headX >= w || headY < 1 || headY >= h) quit = true;
     return quit;
 }
 void Next(int tailX,int tailY, int n,int p=0)
@@ -80,8 +113,8 @@ void Next(int tailX,int tailY, int n,int p=0)
 
 void PrintGameField()
 {
-    for (int y = 0; y < h; y++)
-        for (int x = 0; x < w; x++)
+    for (int y = 0; y <= h; y++)
+        for (int x = 0; x <= w; x++)
         {
             Console.SetCursorPosition(x, y);
             //System.Threading.Thread.Sleep(1);
@@ -95,7 +128,7 @@ void PrintGameField()
                     break;
                 case 1:
                     Console.WriteLine('1');
-                    break;
+                    break;             
                 default:
                     //Console.WriteLine(GameField[x,y]);
                     Console.WriteLine('█');
@@ -160,15 +193,17 @@ void KeyboardUpdate()
 
 
 Init();
-timer.Elapsed += Timer_Elapsed;
+SplashScreen();
+//Console.ReadKey();
+//timer.Elapsed += Timer_Elapsed;
 
 
-PrintGameField();
+//PrintGameField();
 
 while (!quit)
 {
     KeyboardUpdate();
     Update();
     PrintGameField();
-    System.Threading.Thread.Sleep(50);
+    //System.Threading.Thread.Sleep(5);
 };
